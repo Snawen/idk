163dc50b0702c84c2c81147f92cc1ba8c84034ed
Snawen <axel.isaia84@gmail.com>
2020-03-24 16:49:26 +0100

    Exercice 2 et 3

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
-    require 'index.php';
-
-
-
-
-
-
+var_dump($_GET);
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <title>Mon livre de recettes</title>
+        <meta charset="utf-8">
+         <link rel="stylesheet" type="text/css" href="css/style.cs s">
+    </head>
+    
+    <body>
+        <header>
+            <?php require "html/menu.html" ?>
+        </header>
+
+        <article>
+            <h2 class="text-center">Suggestion du jour</h2>
+            <ul class="row items">
+                <li class="img-bullet col-4 text-center"><img src="img/entree3.jpg" alt="Photo de l'entrée 3"><br><a href ="recettes/entrees/entree3.html">Entrée 3</a></li>
+                <li class="img-bullet col-4 text-center"><img src="img/plat1.jpg" alt="Photo du plat 1"><br><a href ="recettes/plats/plat1.html">Plat 1</a></li>
+                <li class="img-bullet col-4 text-center"><img src="img/dessert1.jpg" alt="Photo du dessert 1"><br><a href ="recettes/desserts/dessert1.html">Dessert 1</a></li>
+            </ul>
+        </article>
+    </body>
+
+</html>
